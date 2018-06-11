bd4cca4cd58fbcbf777ea46714ecd7f49011aa4f
Rian Alves <Rian Alves@RIAN>
2018-06-11 19:11:51 -0300

    >Exclusão de Comentários

--- src/main/java/br/ufjf/dcc192/mineradorgithub/Modelos/Colaboradores.java
+++ src/main/java/br/ufjf/dcc192/mineradorgithub/Modelos/Colaboradores.java
@@ -27,61 +27,36 @@ public class Colaboradores {
         this.localizacao = localizacao;
         this.imagem_URL = imagem_URL;
     }
-    
-    
-
-    /**
-     * @return the nome
-     */
+  
     public String getNome() {
         return nome;
     }
 
-    /**
-     * @param nome the nome to set
-     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    /**
-     * @return the email
-     */
     public String getEmail() {
         return email;
     }
 
-    /**
-     * @param email the email to set
-     */
-    public void setEmail(String email) {
+     public void setEmail(String email) {
         this.email = email;
     }
 
-    /**
-     * @return the contribuicao
-     */
     public int getContribuicao() {
         return contribuicao;
     }
 
-    /**
-     * @param contribuicao the contribuicao to set
-     */
     public void setContribuicao(int contribuicao) {
         this.contribuicao = contribuicao;
     }
 
-    /**
-     * @return the localizacao
-     */
+   
     public String getLocalizacao() {
         return localizacao;
     }
 
-    /**
-     * @param localizacao the localizacao to set
-     */
     public void setLocalizacao(String localizacao) {
         this.localizacao = localizacao;
     }
